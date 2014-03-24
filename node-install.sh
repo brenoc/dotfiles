e3bd88131453a1d3d88484c1f947a389b28b29f6
Breno Calazans <brenocalazans@gmail.com>
2014-03-24 17:01:08 -0300

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+cd ~/Downloads/
 ver="0.10.26"
 echo Installing node version $ver
 wget http://nodejs.org/dist/v$ver/node-v$ver.tar.gz
 tar -zxf node-v$ver.tar.gz
 cd node-v$ver
-sudo ./configure && sudo make && sudo make install
+./configure && sudo make && sudo make install
 sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/node
 sudo npm i -g coffee-script less grunt-cli phantomjs karma bower bower-installer express
