de0cfc15b29e9c1206055dd322f5eace1db0fc28
Breno Calazans <brenocalazans@gmail.com>
2014-04-16 10:07:16 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install numlockx build-essential g++ git xclip curl unrar-free
+sudo apt-get -y install numlockx build-essential g++ git xclip curl unrar
 sudo sed -i 's|^exit 0.*$|# Numlock enable\n[ -x /usr/bin/numlockx ] \&\& numlockx on\n\nexit 0|' /etc/rc.local
 
 sudo update-alternatives --config editor
